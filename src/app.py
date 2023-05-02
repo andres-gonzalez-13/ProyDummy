6768c564b42c6950a9e76184fc0b84fdfba23273
andres-gonzalez-13 <andrecksantiago@gmail.com>
2023-05-02 10:26:59 -0500

    lectura de datos de login

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 from config import config
 
@@ -6,10 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    if request.method!='POST':
-        return render_template('auth/stockpage.html')
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        print(request.form['username'])
+        print(request.form['password'])
+        if request.form['username'] == 'andres.gonzalez04@uptc.edu.co':
+            return render_template('auth/stockpage.html')
+        return render_template('auth/login.html')
     else:
         return render_template('auth/login.html')
+
+
+@app.route('/stock')
+def stock():
+    return render_template('auth/stockpage.html')
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
