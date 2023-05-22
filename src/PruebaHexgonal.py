95f0db016b7e7264abd8329a3a696032eeafbe6b
andres-gonzalez-13 <andrecksantiago@gmail.com>
2023-05-22 17:40:56 -0500

    prueba de conexion de con mysql

--- src/PruebaHexgonal.py
+++ src/PruebaHexgonal.py
@@ -4,7 +4,8 @@ from services.UserService import UserService
 user_repository = DatabaseUserRepository()
 user_service = UserService(user_repository)
 
-user = user_service.get_user_by_email_and_password("john.doe@example.com", "password123")
+#user = user_service.get_user_by_email_and_password("johndoe@example.com", "password123")
+user = user_service.get_user_by_email_and_password("andres.gonzalez04@uptc.edu.co", "mimimimi")
 
 if user is None:
     print("no existe el usuario")
--- src/portAdapters/DatabaseUsersRepository.py
+++ src/portAdapters/DatabaseUsersRepository.py
@@ -2,6 +2,16 @@ from typing import Optional
 from interfaces.UserRepository import UserRepository
 from models.User import User
 
+import mysql.connector
+
+# Conectarse a la base de datos
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="mimimimi",
+  database="ahorcado"
+)
+
 class DatabaseUserRepository(UserRepository):
     def get_user_by_email_and_password(self, email: str, password: str) -> Optional[User]:
         # Lógica para consultar el usuario por email y contraseña en la base de datos
@@ -12,5 +22,15 @@ class DatabaseUserRepository(UserRepository):
 
         if email == "":
             user = None
-        
+        else:
+            cursor = db.cursor()
+            # Ejecutar una consulta
+            query = "SELECT * FROM user WHERE email = %s"
+            cursor.execute(query, (email,))
+            # Obtener los resultados
+            result = cursor.fetchone()
+            if result:
+                user = User(result[0],result[1],result[2],result[3])
+            else:
+                user = None
         return user
