aed918549f9e36619c5cf88f614479acc84426ed
andres-gonzalez-13 <andrecksantiago@gmail.com>
2023-05-23 21:19:18 -0500

    interface create product

--- src/interfaces/ProductInterface.py
+++ src/interfaces/ProductInterface.py
@@ -4,3 +4,6 @@ from models.Product import Product
 class ProductInterface:
     def get_available_products(self) -> List[Product]:
         raise NotImplementedError
+    
+    def create_product(self, id: str, name: str, price: float, units: int):
+        raise NotImplementedError
